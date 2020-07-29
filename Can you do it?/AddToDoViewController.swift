202ffb9200fc0967f867e707a2acf4ac6364f3d8
Valentina Phung <vp.valentinaphung@gmail.com>
2020-07-29 14:41:36 -0700

    000

---
+++ Can you do it?/AddToDoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AddToDoViewController.swift
+//  Can you do it?
+//
+//  Created by Valentina Phung on 7/29/20.
+//  Copyright © 2020 Madison Li. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    
+    var previousVC = ToDoTableViewController()
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = ToDo()
+        
+        if let titleText = titleTextField.text {
+            toDo.name = titleText
+            toDo.important = importantSwitch.isOn
+        }
+        previousVC.toDos.append(toDo)
+        previousVC.tableView.reloadData()
+        navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Can you do it?/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  Can you do it?
+//
+//  Created by Valentina Phung on 7/29/20.
+//  Copyright © 2020 Madison Li. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+   
+    var name = ""
+    var important = false
+
+}
+
